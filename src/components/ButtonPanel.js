7bbaf7106fd1b918ddbccf8d2e0782dd5ee518c8
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-04 11:49:21 -0500

    Add the buttons to the App

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Button from './Button';
 
+const buttons = [
+  { name: 'group1', value: ['AC', '+/-', '%', '÷'] },
+  { name: 'group2', value: ['7', '8', '9', 'X'] },
+  { name: 'group3', value: ['4', '5', '6', '-'] },
+  { name: 'group4', value: ['1', '2', '3', '+'] },
+  { name: 'group5', value: ['0', '.', '='] },
+];
+
 const ButtonPanel = () => (
   <div>
-    <Button buttonName={1} />
+    {buttons.map(item => (
+      <div key={item.name}>
+        {item.value.map(itemButton => <Button buttonName={itemButton} key={itemButton} />)}
+      </div>
+    ))}
+
   </div>
 );
 
