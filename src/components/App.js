dd393055b553901e58c9cafbc5392bf2e98038df
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-04 08:53:18 -0500

    Add a basic structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>Hello from App component</div>
+    );
+  }
+}
+
+export default App;
