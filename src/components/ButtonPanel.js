9d2dd0a8270379aa4d7d698256bad079f35dfbd0
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-04 11:56:11 -0500

    Change some values

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Button from './Button';
 
 const buttons = [
-  { name: 'group1', value: ['AC', '+/-', '%', '÷'] },
-  { name: 'group2', value: ['7', '8', '9', 'X'] },
-  { name: 'group3', value: ['4', '5', '6', '-'] },
-  { name: 'group4', value: ['1', '2', '3', '+'] },
-  { name: 'group5', value: ['0', '.', '='] },
+  { name: 'group1', values: ['AC', '+/-', '%', '÷'] },
+  { name: 'group2', values: ['7', '8', '9', 'X'] },
+  { name: 'group3', values: ['4', '5', '6', '-'] },
+  { name: 'group4', values: ['1', '2', '3', '+'] },
+  { name: 'group5', values: ['0', '.', '='] },
 ];
 
 const ButtonPanel = () => (
   <div>
     {buttons.map(item => (
       <div key={item.name}>
-        {item.value.map(itemButton => <Button buttonName={itemButton} key={itemButton} />)}
+        {item.values.map(itemButton => <Button buttonName={itemButton} key={itemButton} />)}
       </div>
     ))}
 
