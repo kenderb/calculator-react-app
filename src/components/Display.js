5bae8b39b48a8e2167e7e890372a05a1dcd035bb
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-05 14:01:44 -0500

    Remove some states

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,22 +1,13 @@
-import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ results }) => {
-  const [calculatorResults, useCalculatorResults] = useState('0');
-
-  useEffect(() => {
-    useCalculatorResults(results);
-  }, [results]);
-
-  return (
-    <div>
-      <p>
-        results:
-      </p>
-      {calculatorResults}
-    </div>
-  );
-};
+const Display = ({ results }) => (
+  <div>
+    <p>
+      results:
+    </p>
+    {results}
+  </div>
+);
 
 Display.propTypes = {
   results: PropTypes.string.isRequired,
