e96b0ab68b3f86b35c5c9b3ea12a77f961e2767c
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-05 13:36:16 -0500

    Add calculate method to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  < >
-    <Display results="0" />
-    <ButtonPanel />
-  </>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: '0',
+      next: '0',
+    };
+  }
+
+  componentDidUpdate() {
+    this.onchange();
+  }
+
+  onchange() {
+    const { total, next } = this.state;
+    this.setState(
+      {
+        total: calculate({ total, next }),
+      },
+    );
+  }
+
+  render() {
+    const { total } = this.state;
+
+    return (
+      <>
+        <Display results={total} />
+        <ButtonPanel />
+      </>
+    );
+  }
+}
 
 export default App;
