5fbbb04596f461c47bf9abc78b8e6f736e935588
kenderb <kenderbolivar@gmail.com>
2021-02-07 01:02:21 -0500

    Add the handleClick method to the ButtonPanel and Button components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const buttonStyle = {
-  fontSize: '2em',
-  padding: '0.8em',
-};
-const Button = ({ buttonName, clickHandler }) => (
-  <button type="button" onClick={() => clickHandler(buttonName)} style={buttonStyle}>
-    {buttonName}
-  </button>
-);
+class Button extends Component {
+  handleClick = buttonName => {
+    const { onClickHandler } = this.props;
+    onClickHandler(buttonName);
+  };
+
+  render() {
+    const buttonStyle = {
+      fontSize: '2em',
+      padding: '0.8em',
+    };
+    const { buttonName } = this.props;
+    return (
+      <button type="button" onClick={() => this.handleClick(buttonName)} style={buttonStyle}>
+        {buttonName}
+      </button>
+    );
+  }
+}
+
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
-  clickHandler: PropTypes.func.isRequired,
+  onClickHandler: PropTypes.func.isRequired,
 };
 export default Button;
