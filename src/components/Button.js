e2d5f5d82c10e204a90f8c6d25dee8eb3b71a1dc
kenderb <kenderbolivar@gmail.com>
2021-02-08 11:03:35 -0500

    Refactor Button and Panel components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends Component {
-  handleClick = buttonName => {
-    const { clickHandler } = this.props;
-    clickHandler(buttonName);
-  };
-
-  render() {
-    const { buttonName } = this.props;
-    return (
-      <button type="button" onClick={() => this.handleClick(buttonName)}>
-        {buttonName}
-      </button>
-    );
-  }
-}
-
+const Button = ({ buttonName, onClickBtn }) => (
+  <button type="button" onClick={() => onClickBtn(buttonName)}>
+    {buttonName}
+  </button>
+);
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
-  clickHandler: PropTypes.func.isRequired,
+  onClickBtn: PropTypes.func.isRequired,
 };
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,38 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-class ButtonPanel extends Component {
-  handleClick = buttonName => {
-    const { clickHandler } = this.props;
-    clickHandler(buttonName);
-  };
+const buttons = [
+  { name: 'group1', values: ['AC', '+/-', '%', '÷'] },
+  { name: 'group2', values: ['7', '8', '9', 'X'] },
+  { name: 'group3', values: ['4', '5', '6', '-'] },
+  { name: 'group4', values: ['1', '2', '3', '+'] },
+  { name: 'group5', values: ['0', '.', '='] },
+];
 
-  render() {
-    const buttons = [
-      { name: 'group1', values: ['AC', '+/-', '%', '÷'] },
-      { name: 'group2', values: ['7', '8', '9', 'X'] },
-      { name: 'group3', values: ['4', '5', '6', '-'] },
-      { name: 'group4', values: ['1', '2', '3', '+'] },
-      { name: 'group5', values: ['0', '.', '='] },
-    ];
-    return (
-      <div>
-        {buttons.map(item => (
-          <div key={item.name}>
-            {item.values.map(itemButton => (
-              <Button
-                buttonName={itemButton}
-                key={itemButton}
-                clickHandler={() => this.handleClick(itemButton)}
-              />
-            ))}
-          </div>
+const ButtonPanel = ({ clickHandler }) => (
+  <div>
+    {buttons.map(item => (
+      <div key={item.name}>
+        {item.values.map(itemButton => (
+          <Button
+            buttonName={itemButton}
+            key={itemButton}
+            onClickBtn={clickHandler}
+          />
         ))}
       </div>
-    );
-  }
-}
+    ))}
+
+  </div>
+);
 
 ButtonPanel.propTypes = {
   clickHandler: PropTypes.func.isRequired,
