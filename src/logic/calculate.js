6c4fb3a2320573f9a8b2d787f3a5ae7c5bd9575f
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-08 09:02:29 -0500

    Add functional componensts to the repo

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,22 +1,45 @@
 import operate from './operate';
 
-const calculate = ({ total, next, operation }) => {
+const calculate = ({ total, next, operation }, typeOperation) => {
   if (operation === '+/-') {
     const newTotal = (total) * -1;
     const newNext = (next) * -1;
-    return { total: newTotal, next: newNext };
+    if (typeOperation) return { next: newNext };
+    return { total: newTotal };
   }
   if (operation === '+'
   || operation === '-'
   || operation === 'X'
-  || operation === '%'
   || operation === '÷') {
-    const totalResult = operate(total, next, operation);
-    return { total: totalResult };
+    return { next: 0, operation };
   }
-  if (typeof (operation) === 'number') {
-    console.log(operation);
-    return operate(operation, next, operation);
+  if (operation === '=') {
+    if (next === '.' || total === '.' || total === 'Math error') return { total: '0', next: null, operation: null };
+    if (next === '=') return operate(total, null, typeOperation);
+    return operate(total, next, typeOperation);
+  }
+  if (operation === 'AC') return { total: '0', next: null, operation: null };
+  if (operation === '%') {
+    if (typeOperation) return operate(next, 100, operation);
+    return operate(total, 100, operation);
+  }
+  if (operation.includes('.')) {
+    const zeroTotal = total || '0';
+    const zeroNext = next || '0';
+    if (zeroTotal.toString().includes('.') && !typeOperation) return { total, next };
+    if (zeroNext.toString().includes('.') && typeOperation) return { total, next, typeOperation };
+  }
+  if (operation.match(/[0-9]|./g)) {
+    const newTotal = '';
+    let newOpera = '';
+    if (typeOperation) {
+      if (next === '0' || next === 0 || next === null || next === '=') newOpera = newTotal + operation;
+      else newOpera = next + operation;
+      return { next: newOpera };
+    }
+    if (total === '0' || total === 0 || total === null || next === '=') newOpera = newTotal + operation;
+    else newOpera = total + operation;
+    return { total: newOpera, next: null };
   }
   return operate(total, next, operation);
 };
