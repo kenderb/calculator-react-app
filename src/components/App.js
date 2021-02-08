54aeb2cf1d2b741d24a3469b12c87c34356d09f9
kenderb <kenderbolivar@gmail.com>
2021-02-08 16:54:44 -0500

    Refactor the router import

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Display from './Display';
 import Quote from './Quote';
