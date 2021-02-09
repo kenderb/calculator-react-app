99888092768cbe0aaa642b178d643de9369fc6a4
Kender Bolivar <kenderbolivar@gmail.com>
2021-02-09 15:37:09 -0500

    Fix the typos on the Calculator JS

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -4,29 +4,29 @@ describe('Calculate method', () => {
   it('Should be a function', () => {
     expect(typeof calculate).toEqual('function');
   });
-  it('Should acept and object and a name as arguments btnName = -', () => {
+  it('Should accept and object and a name as arguments btnName = -', () => {
     expect(calculate({ total: 30, next: 2, operation: '-' })).toEqual({ next: '0', operation: '-', total: 30 });
   });
-  it('Should acept and object and a name as arguments btnName = X', () => {
+  it('Should accept and object and a name as arguments btnName = X', () => {
     expect(calculate({ total: 30, next: 2, operation: 'X' })).toEqual({ next: '0', operation: 'X', total: 30 });
   });
-  it('Should acept and object and a name as arguments btnName = +', () => {
+  it('Should accept and object and a name as arguments btnName = +', () => {
     expect(calculate({ total: 30, next: 2, operation: '+' })).toEqual({ next: '0', operation: '+', total: 30 });
   });
-  it('Should acept and object and a name as arguments btnName = %', () => {
+  it('Should accept and object and a name as arguments btnName = %', () => {
     expect(calculate({ total: 30, next: 2, operation: '%' })).toEqual({ total: '0.3', next: '=', operation: null });
   });
-  it('Should acept and object and a name as arguments btnName = ÷', () => {
+  it('Should accept and object and a name as arguments btnName = ÷', () => {
     expect(calculate({ total: 30, next: 2, operation: '÷' })).toEqual({ next: '0', operation: '÷', total: 30 });
   });
-  it('Should acept and object and a name as arguments btnName = e', () => {
+  it('Should accept and object and a name as arguments btnName = e', () => {
     expect(calculate({ total: 30, next: 2, operation: 'e' })).toEqual({ next: null, total: '30e' });
   });
 
-  it('should use the buttonName argument to mutate the calculator’s data object con un valor negativo', () => {
+  it('should use the buttonName argument to mutate the calculator’s data object with a negative value', () => {
     expect(calculate({ total: 30, next: -1, operation: '+/-' })).toEqual({ total: '-30' });
   });
-  it('should use the buttonName argument to mutate the calculator’s data object con un valor positivo', () => {
+  it('should use the buttonName argument to mutate the calculator’s data object with a positive value.', () => {
     expect(calculate({ total: 30, next: 1, operation: '+/-' })).toEqual({ total: '-30' });
   });
 });
